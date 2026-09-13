1ff8a0e67c50550331edaedf11fef6d608236886
sona9090 <sonasarkisian90@gmail.com>
2026-09-13 13:14:25 +0400

    feat: avatar, menu generation, background animation, first test section

---
+++ script.js
@@ -0,0 +1,56 @@
+const starryAnimation = () => {
+	const starsSpace = document.querySelector("#stars");
+
+	if (!starsSpace) return;
+
+	const animateStars = () => {
+		document.querySelectorAll(".star").forEach((star) => {
+			star.style.top = `${Math.random() * starsSpace.clientHeight}px`;
+			star.style.left = `${Math.random() * starsSpace.clientWidth}px`;
+		});
+	};
+
+	const generateStars = () => {
+		const star = `<div class="star"></div>`;
+		starsSpace.innerHTML = star.repeat(100);
+
+		// Первое положение
+		animateStars();
+
+		// Ждём, пока браузер его реально отрисует
+		requestAnimationFrame(() => {
+			requestAnimationFrame(() => {
+				animateStars();
+			});
+		});
+	};
+
+	generateStars();
+
+	setInterval(animateStars, 8000);
+};
+
+const menuData = [
+	{ name: "About me", link: "" },
+	{ name: "Contacts", link: "" },
+	{ name: "Skills", link: "" },
+	{ name: "Code examples", link: "" },
+	{ name: "Experience", link: "" },
+	{ name: "Education", link: "" },
+	{ name: "English level", link: "" }
+];
+
+const generateMenu = () => {
+	const menu = document.getElementById("menu");
+
+	const menuItems = menuData
+		.map((item) => `<li><a href="${item.link}">${item.name}</a></li>`)
+		.join("");
+
+	menu.innerHTML = menuItems;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	starryAnimation();
+	generateMenu();
+});
